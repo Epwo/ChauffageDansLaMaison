e6ae988c94356ccb11e1389d885c606abb59faa7
Epwo <ewann.postic@hotmail.fr>
2023-03-06 12:07:21 +0100

    C'est le projet d'info de base

---
+++ define.h
@@ -0,0 +1,14 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct {
+	float interieure;
+	float exterieure;
+}temp_t;
+
+
+#endif
---
+++ visualisationT.c
@@ -0,0 +1,21 @@
+#include "visualisationT.h"
+FILE *f;
+void visualisationT(temp_t myTemp){
+
+}
+void EcrireData(temp_t TempInt,temp_t TempExt){
+    if (access(fname, F_OK) == 0) {
+        // file exists
+        f = fopen("IHM/data.txt","w");
+        fprintf(f,"%d \n",TempExt);
+        fprintf(f,"%d\n",TempInt);
+        printf("Your boolean variable is: %s", x ? "true" : "false");
+
+    } else {
+        // file doesn't exist
+        printf("VisualT : Error, verrou exists. cant use data.txt");
+    }
+
+
+
+}
