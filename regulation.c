07b5b94f4b854f858f7c419b1ea65653d89d0102
kozi <evan.kozierow@gmail.com>
2023-03-07 10:21:43 +0100

    Premier programme réussi de lapart de kozi

--- regulation.c
+++ regulation.c
@@ -2,15 +2,21 @@
 	 #include <stdlib.h>
     #include "regulation.h"
 	 
-	float regulationTest(int regul,float consigne,float* tabT, int nT){
-		float cmd;
-        if((*tabT-consigne<0)){ //Si temperature intérieur < consigne : ajout de temperature
-            cmd = 40.0; //Puissance de chauffage
+	float regulationTest(int regul,float consigne,float* tabT, int nT) {
+        float cmd;
+        int i;
+        if (regul == 1) {
+            for (i = 0; i < nT; i++) {
+                if ((tabT[i] - consigne < 0)) { //Si temperature intérieur < consigne : ajout de temperature
+                    cmd = 40; //Puissance de chauffage
+                    //printf("la temperature pris en compte est : %f\n", tabT[0]);
+                } else {
+                    cmd = 0; //Puissance coupée si consigne atteinte
+                    //printf("la temperature pris en compte est : %f\n", tabT[0]);
+                }
+            }
         }
-		else{
-            cmd = 0;
-        }
-		return cmd;
-	}
+        return cmd;
+    }
     
   
