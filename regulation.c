7a2d92f31d8becc8ce6611a8dbf8f21a30c96e2f
kozi <evan.kozierow@gmail.com>
2023-03-07 08:56:19 +0100

    trucmuch

--- regulation.c
+++ regulation.c
@@ -4,7 +4,12 @@
 	 
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
 		float cmd;
-		
+        if((*tabT-consigne<0)){ //Si temperature intérieur < consigne : ajout de temperature
+            cmd = 40.0; //Puissance de chauffage
+        }
+		else{
+            cmd = 0;
+        }
 		return cmd;
 	}
     
