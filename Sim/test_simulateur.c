41222ab74d4629abb165752352977bed6d5c00f5
Epwo <ewann.postic@hotmail.fr>
2023-03-09 18:22:31 +0100

    latest simu test

--- Sim/test_simulateur.c
+++ Sim/test_simulateur.c
@@ -4,18 +4,27 @@
 #include "visualisationC.h"
 #include "visualisationT.h"
 #include "consigne.h"
+#include "regulation.h"
+#include "consigne.h"
+#include "regulation.h"
 
 int main(){
 
 	temp_t temperature;
+    float Tabf[31];
+    Tabf[0]= 10;
 	temperature.exterieure = 14.0;
 	temperature.interieure = 15.0;
 	struct simParam_s*  monSimulateur_ps = simConstruct(temperature); // creation du simulateur, puissance intialis�e � 0%
 	int i=0; // increment de boucle
-	float puissance = 70.0; // puissance de chauffage
-	for(i=0;i< 30;i++){
+	float puissance = 0; // puissance de chauffage
+	for(i=1;i< 31;i++){
+        Tabf[i] = temperature.interieure;
 		temperature=simCalc(puissance,monSimulateur_ps); // simulation de l'environnement
-        visualisationT(temperature);
+        //visualisationT(temperature);
+        //REVENIR ICI
+        puissance = regulationTest(2, 19,Tabf,i);
+        //visualisationC(puissance);
         sleep(1);
 	}
 	simDestruct(monSimulateur_ps); // destruction de simulateur
